4a49d2f63d744d43c37f76f1d1130164b45ed9dd
anubhavpy291 <8840nikita@gmail.com>
2026-04-28 18:00:01 +0530

    search

--- af.py
+++ af.py
@@ -27,14 +27,14 @@ class LinkList:
         new_tail = node(value)
         if self.head == None:
             self.head = new_tail
-            self.n += 1
-
+            self.n  = self.n + 1
+            return
         curr = self.head
         while curr.next != None:
             curr = curr.next
         
         curr.next = new_tail
-        self.n += 1
+        self.n  = self.n + 1
     def insert_after(self,after,value):
         new_node = node(value)
         curr = self.head
@@ -45,16 +45,63 @@ class LinkList:
         if curr != None:
             new_node.next = curr.next
             curr.next = new_node
+            self.n  = self.n + 1
         else:
             print("invalid after")
-            
+    def clear(self):
+        self.head =  None
+        self.n = 0
+    def clear_head(self):
+        if self.head == None:
+            print("empty ListList")
+            return
+        new_head = self.head.next
+        self.head = None
+        self.head = new_head
+        self.n  = self.n - 1
+    def clear_tail(self):
+        if self.head == None:
+            return 'empty ll'
+        curr = self.head
+        if curr.next == None:
+            self.clear_head()
+            return
+        if curr.next.next != None:
+            curr = curr.next
+        curr.next = None
+        self.n  = self.n - 1
+    def remove(self,value):
+        if self.head == None:
+            print("empty ll")
+            return
+        if self.head.data == value:
+            self.clear_head()
+            return
+        curr = self.head
+        while curr != None:
+            if curr.next.data == value:
+                curr.next = curr.next.next
+                self.n = self.n - 1
+                break
+            curr = curr.next
+    def search(self,value):
+        pos = 0
+        curr = self.head 
+        while curr != None:
+            if curr.data == value:
+                return pos
+            curr = curr.next
+            pos = pos + 1
+            return 'data not found'
 l = LinkList()
-l.insert_head(1)
+l.insert_head(5)
 l.insert_tail(2)
 
 l.insert_tail(2)
 l.insert_tail(3)
 l.insert_tail(4)
 l.insert_after(2,67)
+l.remove(67)
+print(l.search(3))
 print(l)
 print(len(l))
