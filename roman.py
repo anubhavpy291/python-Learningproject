06468df4e69e18fbc3fe6484c3da19da09fe95b6
anubhavpy291 <8840nikita@gmail.com>
2026-05-03 16:55:48 +0530

    query

--- roman.py
+++ roman.py
@@ -1,13 +1,18 @@
 d = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-s = "XXX"
-n = len(s)
+s = "MCMXCIV"
+
+n = (len(s) - 1)
 t = 0
-for i in range(n-1):
-    for c in range(i+1,n):
-        a = d[s[i]]
-        b = d[s[c]]
-        if a < b:
-            t += b - a
-        else:
-            t += a
+i = 0
+while n >= 0:
+    a = d[s[n]]
+    if(n != 0):
+        b = d[s[n-1]]
+    
+    if a > b and n != 0:
+        t = t + (a - b)
+        n -= 2
+    else:
+        t = t + a
+        n -= 1
 print(t)
