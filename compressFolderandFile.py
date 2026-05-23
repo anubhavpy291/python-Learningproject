4636db3f5239fff699d4cdd72edfe5056e49e722
anubhavpy291 <8840nikita@gmail.com>
2026-05-23 16:35:13 +0530

    compressfolderandfile

--- compressFolderandFile.py
+++ compressFolderandFile.py
@@ -13,12 +13,19 @@ if args.c:
     if os.path.exists(args.c):
         
         with zipfile.ZipFile(f"{name}.zip","w",compression=zipfile.ZIP_DEFLATED) as  z:
-            z.write(args.c,arcname=os.path.basename(args.c))
-            print("compress complete")
+            if os.path.isfile(args.c):
+                z.write(args.c,arcname=os.path.basename(args.c))
+                print("compress complete")
+            else:
+                for root, dirs, files in os.walk(args.c):
+                    for file in files:
+                        file_path = os.path.join(root,file)
+                        arcname = os.path.relpath(file_path,args.c)
+                        z.write(file_path,arcname)
+       
 elif args.d:
     name = os.path.splitext(args.d)[0]
     if os.path.exists(args.d):
-        
         with zipfile.ZipFile(args.d,"r") as z:
             z.extractall(name)
             print("extract complete")
